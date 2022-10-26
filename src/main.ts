0bb00c9c43623d0e6fbd5b572411878ac7a9b25c
Benedek Ákos Marcell <benedek.akos.marcell@gmail.com>
2022-10-26 14:30:34 +0200

    main function

---
+++ src/main.ts
@@ -0,0 +1,40 @@
+let namePattern = /[a-z]{1,}/gi;
+let countryPattern = /[a-z]{1,}/gi;
+let cityPattern = /[a-z]{1,}/gi;
+let housePattern = /[a-z 0-9]{1,}/gi;
+let zipPattern = /[A-Z0-9]{1,}/g;
+let bankPattern = /[0-9]{1,4}-[0-9]{1,4}-[0-9]{1,4}-[0-9]{1,4}/;
+let codePattern = /[0-9]{3,3}/;
+let bankkardnamePattern = /a-z /gi
+function fizetes(){
+let hiba :number = 0;
+if(namePattern.test("")==false){
+    hiba++;
+}
+if(countryPattern.test("")==false){
+    hiba++;
+}  
+if(cityPattern.test("")==false){
+    hiba++;
+}  
+if(housePattern.test("")==false){
+    hiba++;
+}  
+if(zipPattern.test("")==false){
+    hiba++;
+}  
+if(bankPattern.test("")==false){
+    hiba++;
+}  
+if(codePattern.test("")==false){
+    hiba++;
+}       
+if(bankkardnamePattern.test("")==false){
+    hiba++;
+}
+if(hiba==0){
+    console.log("Sikeres tranzakció");
+}else{
+    console.log("Hibás adat!")
+}
+};
